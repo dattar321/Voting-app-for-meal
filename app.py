de99edf9ecb95104b9c1f90d90f12053d2362976
dattar321 <dattar321@gmail.com>
2024-02-01 03:23:51 +0600

    Implemented admin panel, added MenuHistory for consecutive orders, and restricted a restaurant from winning three days in a row.

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from flask import Flask, render_template, url_for, redirect, flash, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, SelectField, validators
 from wtforms.validators import InputRequired, Length, ValidationError
 from flask_bcrypt import Bcrypt
-from datetime import datetime
+from datetime import datetime, timedelta, date
+from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
@@ -33,13 +34,18 @@ class Restaurant(db.Model):
     streak = db.Column(db.Integer, default=0) 
     menus = db.relationship('Menu', backref='restaurant', lazy=True)
 
-
 class Menu(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date, default=datetime.now().date())
-    name = db.Column(db.String(50), unique=True, nullable=False)
+    name = db.Column(db.String(80), unique=True, nullable=False)
     vote = db.Column(db.Integer, default=0)
     description = db.Column(db.String(255), nullable=False)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'), nullable=False)
+
+class MenuHistory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    datetime = db.Column(db.DateTime, default=datetime.now)
+    menu_id = db.Column(db.Integer, db.ForeignKey('menu.id'), nullable=False)
     restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'), nullable=False)
 
 
@@ -49,18 +55,31 @@ class RegisterForm(FlaskForm):
     submit = SubmitField('Register')
 
     def validate_username(self, username):
-        existing_user_username = Employee.query.filter_by(
-            username=username.data).first()
+        existing_user_username = Employee.query.filter_by(username=username.data).first()
         if existing_user_username:
             flash('That username already exists. Please choose a different one.')
-            raise ValidationError(
-                'That username already exists. Please choose a different one.')
-
+            raise ValidationError('That username already exists. Please choose a different one.')
 
 class LoginForm(FlaskForm):
     username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
     password = PasswordField(validators=[InputRequired(), Length(min=6, max=20)], render_kw={"placeholder": "Password"})
     submit = SubmitField('Login')
+
+class AddRestaurantForm(FlaskForm):
+    name = StringField(validators=[InputRequired(), Length(min=10, max=30)], render_kw={"placeholder": "Enter Restaurant Name"})
+    submit = SubmitField('Add Restaurant')
+
+class AddMenuForm(FlaskForm):
+    name = StringField(validators=[InputRequired(), Length(min=6, max=30)], render_kw={"placeholder": "Enter Restaurant Name"})
+    description = StringField(validators=[InputRequired(), Length(min=10, max=80)], render_kw={"placeholder": "Enter Description"})
+    submit = SubmitField('Add Menu')
+
+class choicesForm(FlaskForm):
+    restaurants = Restaurant.query.all()
+    choices = []
+    for i in restaurants:
+        choices.append((i.id,i.name))
+    dropdown = SelectField('Choose an option', choices=choices, validators=[validators.InputRequired()])
 
 @app.route('/')
 def home():
@@ -90,15 +109,11 @@ def submit_vote():
     restaurants,all_menus,_ = get_restaurant_and_menus()
     menu_id = request.form.get('selectedMenuId')
     selected_menu = Menu.query.get(menu_id)
-    print('voteee   ',selected_menu.name, selected_menu.vote)
     if selected_menu:
-        # Increment the vote count
-        print('voteee   ',selected_menu.vote)
         selected_menu.vote = selected_menu.vote + 1
         db.session.commit()
         highest_voted_menu = Menu.query.order_by(Menu.vote.desc()).first().name
         return render_template('vote.html', user=current_user.username, restaurants=restaurants, all_menus=all_menus, selected_menu = highest_voted_menu)
-    print('voteee   ',selected_menu.name, selected_menu.vote)
     return redirect(url_for('vote'))
 
 
@@ -122,20 +137,51 @@ def admin():
 @app.route('/addRestaurant', methods=['GET', 'POST'])
 @login_required
 def addRestaurant():
-    return render_template('admin.html')
+    form = AddRestaurantForm()
+    if form.name.data:
+        existing_restaurant = Restaurant.query.filter_by(name=form.name.data).first()
+        if not existing_restaurant:
+            new_restaurant = Restaurant(name=form.name.data)
+            db.session.add(new_restaurant)
+            db.session.commit()
+            return redirect(url_for('admin'))
+    return render_template('addRestaurant.html', form=form)
 
 @app.route('/addMenu', methods=['GET', 'POST'])
 @login_required
-def addMenu():
-    return render_template('admin.html')
+def addMenu():    
+    form = AddMenuForm()
+    choices = choicesForm()
+    if choices.dropdown.data and form.name.data:
+        existing_menu = Menu.query.filter_by(name=form.name.data).first()
+        if not existing_menu:
+            new_menu = Menu(date = datetime.now().date(),name = form.name.data,restaurant_id = choices.dropdown.data, description=form.description.data,)
+            db.session.add(new_menu)
+            db.session.commit()
+            return redirect(url_for('admin'))
+    return render_template('addMenu.html',form=form, choices=choices)
+
+@app.route('/mealConfirm', methods=['GET', 'POST'])
+@login_required
+def mealConfirm():
+    highest_voted_menu = Menu.query.order_by(Menu.vote.desc()).first()
+    today_date = date.today()
+    today_in_menu_history = MenuHistory.query.filter(MenuHistory.datetime >= today_date, MenuHistory.datetime < today_date + timedelta(days=1)).first()
+    if not today_in_menu_history:
+        first = MenuHistory.query.order_by(MenuHistory.datetime.desc()).first().restaurant_id
+        second = MenuHistory.query.order_by(MenuHistory.datetime.desc()).offset(1).first().restaurant_id
+        if first and second and first == second and first == highest_voted_menu.restaurant_id:
+            highest_voted_menu = Menu.query.order_by(Menu.vote.desc()).offset(1).first()
+        new_menu_history = MenuHistory(datetime=today_date, menu_id=highest_voted_menu.id, restaurant_id=highest_voted_menu.restaurant_id)
+        db.session.add(new_menu_history)
+        db.session.commit()
+    return render_template('mealconfirm.html',menu = highest_voted_menu.name)
 
 @app.route('/logout', methods=['GET', 'POST'])
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('login'))
-
-
 
 def get_restaurant_and_menus():
     restaurants = Restaurant.query.all()
@@ -146,6 +192,18 @@ def get_restaurant_and_menus():
         menus = Menu.query.filter_by(restaurant_id=restaurant.id).all()
         all_menus[restaurant.id] = menus
     return restaurants,all_menus,highest_voted_menu
+
+def reset_votes():
+    current_datetime = datetime.now()
+    reset_time = current_datetime - timedelta(hours=24) 
+    menus_to_reset = Menu.query.filter(Menu.date <= reset_time).all()
+    for menu in menus_to_reset:
+        menu.vote = 0
+    db.session.commit()
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(reset_votes, trigger='interval', hours=24)  # Run every 24 hours
+scheduler.start()
 
 if __name__ =='__main__':
     app.run(debug=True)

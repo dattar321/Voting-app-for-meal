fd4d7c0f77d1e0a265744b2665947902c42bebc9
dattar321 <dattar321@gmail.com>
2024-01-31 22:34:57 +0600

    implemented Voting System and Admin Panel UI for Displaying Highest Voted Menu

--- app.js
+++
@@ -1,8 +0,0 @@
-// Base URL for the backend API
-const baseUrl = 'http://localhost:5000';
-
-// Function to handle user login
-async function login() {
-  
-}
-
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, redirect, flash
+from flask import Flask, render_template, url_for, redirect, flash, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from flask_wtf import FlaskForm
@@ -20,7 +20,7 @@ login_manager.login_view = 'login'
 
 @login_manager.user_loader
 def load_user(user_id):
-    return Employee.query.get(int(user_id))
+    return db.session.query(Employee).get(int(user_id))
 
 class Employee(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -38,6 +38,7 @@ class Menu(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date, default=datetime.now().date())
     name = db.Column(db.String(50), unique=True, nullable=False)
+    vote = db.Column(db.Integer, default=0)
     description = db.Column(db.String(255), nullable=False)
     restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'), nullable=False)
 
@@ -80,16 +81,25 @@ def login():
 @app.route('/vote', methods=['GET', 'POST'])
 @login_required
 def vote():
-    restaurants = Restaurant.query.all()
-    all_menus = {}
-
-    for restaurant in restaurants:
-        menus = Menu.query.filter_by(restaurant_id=restaurant.id).all()
-        all_menus[restaurant.id] = menus
-
-    return render_template('vote.html', user=current_user.username, restaurants=restaurants, all_menus=all_menus)
+    restaurants,all_menus,highest_voted_menu = get_restaurant_and_menus()
+    return render_template('vote.html', user=current_user.username, restaurants=restaurants, all_menus=all_menus, selected_menu = highest_voted_menu)
 
 
+@app.route('/submit-vote', methods=['POST'])
+def submit_vote():
+    restaurants,all_menus,_ = get_restaurant_and_menus()
+    menu_id = request.form.get('selectedMenuId')
+    selected_menu = Menu.query.get(menu_id)
+    print('voteee   ',selected_menu.name, selected_menu.vote)
+    if selected_menu:
+        # Increment the vote count
+        print('voteee   ',selected_menu.vote)
+        selected_menu.vote = selected_menu.vote + 1
+        db.session.commit()
+        highest_voted_menu = Menu.query.order_by(Menu.vote.desc()).first().name
+        return render_template('vote.html', user=current_user.username, restaurants=restaurants, all_menus=all_menus, selected_menu = highest_voted_menu)
+    print('voteee   ',selected_menu.name, selected_menu.vote)
+    return redirect(url_for('vote'))
 
 
 @ app.route('/register', methods=['GET', 'POST'])
@@ -104,12 +114,38 @@ def register():
 
     return render_template('register.html', form=form)
 
+@app.route('/admin', methods=['GET', 'POST'])
+@login_required
+def admin():
+    return render_template('admin.html')
+
+@app.route('/addRestaurant', methods=['GET', 'POST'])
+@login_required
+def addRestaurant():
+    return render_template('admin.html')
+
+@app.route('/addMenu', methods=['GET', 'POST'])
+@login_required
+def addMenu():
+    return render_template('admin.html')
+
 @app.route('/logout', methods=['GET', 'POST'])
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('login'))
 
+
+
+def get_restaurant_and_menus():
+    restaurants = Restaurant.query.all()
+    highest_voted_menu = Menu.query.order_by(Menu.vote.desc()).first().name
+    all_menus = {}
+
+    for restaurant in restaurants:
+        menus = Menu.query.filter_by(restaurant_id=restaurant.id).all()
+        all_menus[restaurant.id] = menus
+    return restaurants,all_menus,highest_voted_menu
 
 if __name__ =='__main__':
     app.run(debug=True)
